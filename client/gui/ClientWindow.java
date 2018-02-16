7383d3f1ff59d683f38a7622aa7ecaf62152eaca
Shilpa Gajanan Bhat <shi.g.bhat@gmail.com>
2018-02-16 00:04:56 -0700

    Client window - first commit

---
+++ client/gui/ClientWindow.java
@@ -0,0 +1,42 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ClientWindow {
+
+	private JFrame frmClient;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClientWindow window = new ClientWindow();
+					window.frmClient.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ClientWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmClient = new JFrame();
+		frmClient.setTitle("Client");
+		frmClient.setBounds(100, 100, 750, 500);
+		frmClient.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
