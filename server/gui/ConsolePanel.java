7afc30168e7877f2d80727b03a568812e2cbde02
KaranBhangal <karansherbhangal@gmail.com>
2018-02-17 21:32:02 -0700

    Add files via upload

---
+++ server/gui/ConsolePanel.java
@@ -0,0 +1,24 @@
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+
+/**
+ * The ConsolePanel implements a JPanel which consists of 
+ * two JLabels. One displays a fixed label name and other displays any input to the 
+ * ConsolePanel.
+ * 
+ * @author bhangal
+ * @version 1.0
+ * @since 2017-02-17
+ *
+ */
+public class ConsolePanel extends JPanel{
+	
+	private JLabel nameLabel,messageLabel;
+	
+	public ConsolePanel(String message) {
+		
+	}
+
+}
