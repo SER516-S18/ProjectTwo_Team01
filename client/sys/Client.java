41a46d4629bba409a8a340d5e0fc2f71adef7782
Shilpa Gajanan Bhat <shi.g.bhat@gmail.com>
2018-02-21 16:17:43 -0700

    Client Socket Functionalities

---
+++ client/sys/Client.java
@@ -0,0 +1,98 @@
+package sys;
+
+/**
+ * 
+ * @author 	Shilpa Bhat
+ * @author 	Group 1 #001 - #013
+ * @since 	FEB 2018
+ * @version	1.0
+ * 
+ * Client class used to connect to server, receive 
+ * numbers on given number of channels.
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.*;
+
+public class Client
+{
+	private static Socket socket;
+	private const string ipAddress = "127.0.0.1";
+	private const int port = 1024;
+	private int channels;
+	
+	public static ArrayList<ArrayList<String>> numberList = new ArrayList<ArrayList<String>>();
+	
+	/**
+	 * Constructor accepts the number of 
+	 * channels through which data has to be 
+	 * received from the server.
+	 * 
+	 * Create lists to store number for each channel.
+	 * 
+	 * @param numChannels - number of channels to receive numbers
+	 * from server
+	 */
+	public Client(int numChannels)
+	{
+		channels = numChannels;
+		for(int channel=0;channel<channels;channel++)
+		{
+			ArrayList<string> channelNumbers = new ArrayList<>(string)();
+			numberList.add(channelNumbers);
+		}
+	}
+	/**
+	 * Creates socket and opens connection with the server
+	 * at ip 'ipAddress' and port number 'port'.
+	 */
+	public void createSocket()
+	{
+		try
+		 {
+			socket = new Socket(ipAddress,port);
+			
+			OutputStream os = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+            
+            bw.write(""+channels);
+            bw.flush();
+            
+            while(true)
+            {
+            	InputStream inputStream =  socket.getInputStream();
+            	InputStreamRenader reader = new InputStreamReader(inputStream);
+            	BufferedReader bufferReader = new BufferedReader(reader);
+            	String message = bufferReader.readLine();
+            	
+            }
+		 }
+		 catch (Exception exception)
+	        {
+	            exception.printStackTrace();
+	        }
+	}
+	
+	/**
+	 * Terminates the connection with server.
+	 */
+	public void closeConnection()
+	{
+        try
+        {
+            socket.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+	}
+	
+}
