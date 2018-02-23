75b6cd7c93886a9ed41b5280fe5880e69384d3c5
Zelin Bao <35466865+baozelin@users.noreply.github.com>
2018-02-22 22:42:00 -0700

    testClient

--- server/Client.java
+++ server/Client.java
@@ -1,21 +1,39 @@
+package ser516.project2.team1.server.sys;
 import java.net.*;
 import java.util.Scanner;
 import java.io.*;
 
-public class Client {
+public class client {
 	public static void main(String[] args) throws UnknownHostException, IOException{
 		
-	 int temp;
+	 String temp;
 	 
 	 //create a socket
-	 Socket s = new Socket("127.0.0.1", 8001);
+	 Socket s = new Socket("10.152.8.87", 8001);
 	 
 	 //receive value from server
 	 Scanner sc1 = new Scanner(s.getInputStream());
-	 temp = sc1.nextInt();
+	 temp = sc1.nextLine();
 	 
-   System.out.println(temp); // print the value which is from server
-		
-	//s.close();   for button to close connect
+     System.out.println(temp); // print the value which is from server
+     //System.out.println("xxx");
+     
+     PrintWriter p = new PrintWriter(s.getOutputStream());
+     p.println("channels=3;");
+     
+     p.flush();
+     
+    // Scanner sc2 = new Scanner(s.getInputStream());
+	 //temp = sc2.nextLine();
+	 //System.out.println("heeloo");
+	 
+	 for(;;){
+		 if(sc1.hasNextLine()){
+			 System.out.println(sc1.nextLine());
+		 }
+	 }
+	 
+	 
+	 
 	}
 }
