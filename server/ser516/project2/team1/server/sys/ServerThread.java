9beffacc6013b622fe303ebcbe4cc42c6c5b5827
CSE360-Team5-Lin <zbao7@asu.edu>
2018-02-23 19:48:12 -0700

    fix indentation

--- server/ser516/project2/team1/server/sys/ServerThread.java
+++ server/ser516/project2/team1/server/sys/ServerThread.java
@@ -1,10 +1,8 @@
 package ser516.project2.team1.server.sys;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Random;
-
 import util.ConsolePanel;
 
 /**
@@ -17,19 +15,18 @@ import util.ConsolePanel;
  * @version	1.0
  * 
  */
-
 public class ServerThread implements Runnable {
 	
 /**
  * Channel ID.
  * Random number which are sent to client
  */
- private Socket socket;
- private PrintWriter out;
- private String channelID;
- private Random randomGenerator;
- private int randomNumber;
- private final int FREQ_THRESHOLD = Server.frequency;
+	private Socket socket;
+	private PrintWriter out;
+	private String channelID;
+	private Random randomGenerator;
+	private int randomNumber;
+	private final int FREQ_THRESHOLD = Server.frequency;
   
   
   /**
@@ -37,19 +34,19 @@ public class ServerThread implements Runnable {
    * @param socket presents current socket
    * @param id presents current channel ID
    */
-  public ServerThread (Socket socket, int id) {
-    this.socket = socket;
-    this.channelID = "channelID_" + id;  
-    randomGenerator = new Random(System.currentTimeMillis());
-  }
+	public ServerThread (Socket socket, int id) {
+		this.socket = socket;
+		this.channelID = "channelID_" + id;  
+		randomGenerator = new Random(System.currentTimeMillis());
+	}
   
   /**
    * Get channel ID
    * @return Channel ID
    */
-  public String getChannelID () {
-    return this.channelID;
-  }  
+	public String getChannelID () {
+		return this.channelID;
+	}  
   
 /**
  * Run method creates several random numbers and send them to client. The console shows current channel ID
@@ -60,29 +57,27 @@ public class ServerThread implements Runnable {
  * @param randomNumber represents random number
  * @param channelID represents channel ID
  */
-  
-  public void run () {
-    try {
-      try {
-        out = new PrintWriter (socket.getOutputStream());
-
-        while (true) {
-          try {
-            randomNumber = randomGenerator.nextInt(Server.max - Server.min) + Server.min;
-            out.println(this.channelID + "=" + randomNumber + ";");
-            out.flush();
+	public void run () {
+		try {
+			try {
+				out = new PrintWriter (socket.getOutputStream());
+				while (true) {
+					try {
+						randomNumber = randomGenerator.nextInt(Server.max - Server.min) + Server.min;
+						out.println(this.channelID + "=" + randomNumber + ";");
+						out.flush();
             
-            ConsolePanel.updateText("sending to " + channelID + " randomNumber " + randomNumber + "\n");
-            Thread.sleep(FREQ_THRESHOLD);
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          }
-        }   
-      } finally {
-        socket.close();
-      }
-    } catch (IOException e){
-      System.out.println (e);
-    }
-  } 
+						ConsolePanel.updateText("sending to " + channelID + " randomNumber " + randomNumber + "\n");
+						Thread.sleep(FREQ_THRESHOLD);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}   
+			} finally {
+				socket.close();
+			}
+		} catch (IOException e){
+			System.out.println (e);
+		}
+	} 
 }
